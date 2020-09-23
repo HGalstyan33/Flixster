4415f24accf0be75a949349b27265e0e14e65cc4
HGalstyan33 <hasmikgalstyan33@gmail.com>
2020-09-23 16:51:30 -0700

    Started on last stretch story (popular movie banner)

--- app/src/main/java/hasmik/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/hasmik/example/flixster/adapters/MovieAdapter.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -19,7 +18,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import hasmik.example.flixster.R;
 import hasmik.example.flixster.models.Movie;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static int NORMAL_MOVIE = 1;
+    private static int POPULAR_MOVIE = 2;
 
     Context context;
     List<Movie> movies;
@@ -32,28 +34,48 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     // Usually involves inflating a layout from XML and returning the holder
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d("MovieAdapter","onCreateViewHolder");
-        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
-        return new ViewHolder(movieView);
+        View movieView;
+        if (viewType == NORMAL_MOVIE) {
+            movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+            return new ViewHolder(movieView);
+        } else {
+            movieView = LayoutInflater.from(context).inflate(R.layout.item_popular_movie, parent, false);
+            return new PopularViewHolder(movieView);
+        }
     }
 
     // Involves populating data into the item through holder
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Log.d("MovieAdapter","onBindViewHolder " + position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Log.d("MovieAdapter","onBindViewHolder (Normal) " + position);
 
         // Get the movie at the passed-in position
         Movie movie = movies.get(position);
 
         // Bind the movie data into the ViewHolder
-        holder.bind(movie);
+        if (getItemViewType(position) == NORMAL_MOVIE) {
+            ((ViewHolder) holder).bind(movie);
+        } else {
+            ((PopularViewHolder) holder).bind(movie);
+        }
     }
 
     // Returns the total count of items in the list
     @Override
     public int getItemCount() {
         return movies.size();
+    }
+
+    // Checks if movie has 7.5 or more stars
+    @Override
+    public int getItemViewType(int position) {
+        Movie movie = movies.get(position);
+        if (movie.getVoteAverage() >= 7.5) {
+            return POPULAR_MOVIE;
+        }
+        return NORMAL_MOVIE;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -83,6 +105,20 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             }
 
             Glide.with(context).load(imageURL).placeholder(R.drawable.loading).into(ivPoster);
+        }
+    }
+
+    public class PopularViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView backdrop;
+
+        public PopularViewHolder(@NonNull View itemView) {
+            super(itemView);
+            backdrop = itemView.findViewById(R.id.idBackdrop);
+        }
+
+        public void bind(Movie movie) {
+            Glide.with(context).load(movie.getBackdropPath()).placeholder(R.drawable.loading).into(backdrop);
         }
     }
 }
--- app/src/main/java/hasmik/example/flixster/models/Movie.java
+++ app/src/main/java/hasmik/example/flixster/models/Movie.java
@@ -13,12 +13,14 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    int voteAverage;
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getInt("vote_average");
     }
 
     public static List<Movie> fromJSONArray (JSONArray movieJSONArray) throws JSONException {
@@ -43,5 +45,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public int getVoteAverage() {
+        return voteAverage;
     }
 }
